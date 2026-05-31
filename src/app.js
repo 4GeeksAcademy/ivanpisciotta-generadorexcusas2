52474dd182b4b180fb5ca2a32ad2dcdd312bc32f
ivanpisciotta <ivan.pisciotta7@gmail.com>
2026-05-31 11:50:52 -0400

    feat: generador de excusas aleatorias

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import "bootstrap";
 import "./style.css";
 
+    window.onload = function() {
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    function getRandomItem(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    let excuse = getRandomItem(who) + ' ' + getRandomItem(action) + ' ' + getRandomItem(what) + ' ' + getRandomItem(when);
+
+    document.getElementById('excuse').innerHTML = excuse;
 };
+
